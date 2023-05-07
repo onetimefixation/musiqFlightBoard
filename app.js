a7f749e4a5a673309b6f5f9e15897c0732ba9079
onetimefixation <onetimefixation@gmail.com>
2023-05-07 19:56:36 -0400

    Heroku 1.3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -9,4 +9,35 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`My Server running on port ${port}`);
+}); */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  // Serve the index.html file
+/*   if (req.url === '/' || req.url === '/index.html') { */
+if (req.url === '/' || req.url === '/hero_musiqBoard.html') {
+    const filePath = path.join(__dirname, 'hero_musiqBoard.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal server error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other requests
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
