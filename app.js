6cdb39e9dd119b68e9f0a352102857b944ce6f30
onetimefixation <onetimefixation@gmail.com>
2023-05-07 20:25:17 -0400

    heroku 1.5

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-/* const express = require('express');
-const app = express();
 
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.listen(port, () => {
-  console.log(`My Server running on port ${port}`);
-}); */
-
+/* 
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -19,7 +8,6 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   // Serve the index.html file
-/*   if (req.url === '/' || req.url === '/index.html') { */
 if (req.url === '/' || req.url === '/hero_musiqBoard.html') {
     const filePath = path.join(__dirname, 'hero_musiqBoard.html');
     fs.readFile(filePath, (err, data) => {
@@ -28,6 +16,62 @@ if (req.url === '/' || req.url === '/hero_musiqBoard.html') {
         res.end('Internal server error');
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other requests
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+ */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/hero_musiqBoard.html') {
+    // Serve the HTML file
+    const filePath = path.join(__dirname, 'hero_musiqBoard.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal server error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/style1.css') {
+    // Serve the CSS file
+    const filePath = path.join(__dirname, 'style1.css');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal server error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/hero_musiqBoard.js') {
+    // Serve the JS file
+    const filePath = path.join(__dirname, 'hero_musiqBoard.js');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal server error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
         res.end(data);
       }
     });
