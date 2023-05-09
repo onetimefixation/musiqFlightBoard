b8b4d2e4b240c02730caaa6f592be7e512b9465c
onetimefixation <onetimefixation@gmail.com>
2023-05-09 17:40:40 -0400

    hero 1.10

--- hero_musiqBoard.js
+++ hero_musiqBoard.js
@@ -1,20 +1,10 @@
-const https = require('https');
-
 const apiUrl = 'https://data.vatsim.net/v3/vatsim-data.json';
 
-https.get(apiUrl, (res) => {
-  let data = '';
-  res.on('data', (chunk) => {
-    data += chunk;
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
   });
-  res.on('end', () => {
-    try {
-      const jsonData = JSON.parse(data);
-      console.log(jsonData);
-    } catch (err) {
-      console.error(err);
-    }
-  });
-}).on('error', (err) => {
-  console.error(err);
-});
